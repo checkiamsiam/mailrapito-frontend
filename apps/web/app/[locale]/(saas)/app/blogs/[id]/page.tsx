11b74ecc203a9475160ef8b32b83128a5efb3b96
STECHNO <saleheen.noor@gmail.com>
2024-04-23 13:03:45 +0500

    added edit blog page

--- apps/web/app/[locale]/(saas)/app/blogs/[id]/page.tsx
+++ apps/web/app/[locale]/(saas)/app/blogs/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { apiClient } from "@shared/lib/api-client";
+import { Button } from "@ui/components/button";
 import { Card } from "@ui/components/card";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function ClientComponent() {
   const params = useParams();
   const id = params.id as string;
+  const router = useRouter();
 
   const { data, isLoading } = apiClient.posts.singlePost.useQuery({
     id,
@@ -20,8 +22,23 @@ export default function ClientComponent() {
   }
 
   return (
-    <Card>
-      {data && <div className="p-4" dangerouslySetInnerHTML={createMarkup()} />}
-    </Card>
+    <>
+      <div className="p- flex justify-end space-x-2 pb-2">
+        <Button
+          onClick={() => {
+            router.push(`/app/blogs/edit-post?id=${id}`);
+          }}
+        >
+          Edit
+        </Button>
+        <Button>Delete</Button>
+      </div>
+      <Card>
+        <div className="p-4">{data?.title}</div>
+        {data && (
+          <div className="p-4" dangerouslySetInnerHTML={createMarkup()} />
+        )}
+      </Card>
+    </>
   );
 }
---
+++ apps/web/app/[locale]/(saas)/app/blogs/edit-post/page.tsx
@@ -0,0 +1,312 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { apiClient } from "@shared/lib/api-client";
+import { Button } from "@ui/components/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@ui/components/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@ui/components/form";
+import { Input } from "@ui/components/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@ui/components/select";
+import { Textarea } from "@ui/components/textarea";
+import { useTranslations } from "next-intl";
+import dynamic from "next/dynamic";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import type { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import "react-quill/dist/quill.snow.css";
+import { z } from "zod";
+const QuillEditor = dynamic(() => import("react-quill"), { ssr: false });
+
+const quillModules = {
+  toolbar: [
+    [{ header: [1, 2, 3, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image"],
+    [{ align: [] }],
+    [{ color: [] }],
+    ["code-block"],
+    ["clean"],
+  ],
+};
+
+const quillFormats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "link",
+  "image",
+  "align",
+  "color",
+  "code-block",
+];
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Required" }),
+  author: z.string().min(1, { message: "Required" }),
+  slug: z.string().min(1, { message: "Required" }),
+  description: z.string().min(1, { message: "Required" }),
+  keywords: z.string().min(1, { message: "Required" }),
+  category: z.enum(["", "MEMBER", "OWNER"]),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export default function EditBlog() {
+  const t = useTranslations();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams.toString());
+  const id = params.get("id") as string;
+  const [content, setContent] = useState<any>("");
+
+  const { data, isLoading } = apiClient.posts.singlePost.useQuery({
+    id,
+  });
+
+  const handleEditorChange = (newContent) => {
+    setContent(newContent);
+  };
+
+  const categoryOptions = [
+    {
+      label: t("settings.team.members.roles.member"),
+      value: "MEMBER",
+    },
+    {
+      label: t("settings.team.members.roles.owner"),
+      value: "OWNER",
+    },
+  ];
+
+  const languageOptions = [
+    {
+      label: "English",
+      value: "en",
+    },
+    {
+      label: "French",
+      value: "fr",
+    },
+    {
+      label: "Spanish",
+      value: "es",
+    },
+    {
+      label: "Arabic",
+      value: "ar",
+    },
+  ];
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      slug: "",
+      author: "",
+      keywords: "",
+      category: "",
+      description: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = async (values) => {
+    console.log("onSubmit: SubmitHandler called");
+
+    if (values.category === "") {
+      alert("Please select a category");
+      return;
+    }
+
+    if (!content) {
+      alert("Content is required");
+      return;
+    }
+    try {
+      await createPostMutation.mutateAsync({
+        ...values,
+        content,
+      });
+      form.reset();
+      setContent("");
+
+      console.log("success");
+    } catch {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      form.setValue("title", data?.title);
+      form.setValue("slug", data?.slug);
+      form.setValue("author", data?.author);
+      form.setValue("keywords", data?.keywords);
+      if (data?.category === "MEMBER") {
+        form.setValue("category", "MEMBER");
+      } else if (data?.category === "OWNER") {
+        form.setValue("category", "OWNER");
+      }
+      form.setValue("description", data?.description);
+      if (data?.content) {
+        setContent(data?.content);
+      }
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Edit Post</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="@container">
+            <div className="">
+              <div className="">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input type="title" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="">
+                <FormField
+                  control={form.control}
+                  name="slug"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Slug</FormLabel>
+                      <FormControl>
+                        <Input type="slug" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="">
+                <FormField
+                  control={form.control}
+                  name="author"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Author</FormLabel>
+                      <FormControl>
+                        <Input type="author" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="">
+                <FormField
+                  control={form.control}
+                  name="keywords"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Keywords</FormLabel>
+                      <FormControl>
+                        <Input type="keywords" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div>
+                <FormField
+                  control={form.control}
+                  name="category"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Category</FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={data?.category ?? ""}
+                        >
+                          <SelectTrigger className="">
+                            <SelectValue />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {categoryOptions.map((option) => (
+                              <SelectItem
+                                key={option.value}
+                                value={option.value}
+                              >
+                                {option.label}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="">
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="h-[500px]">
+                <FormLabel>Content</FormLabel>
+                <div className="h-full w-full">
+                  <QuillEditor
+                    value={content}
+                    onChange={handleEditorChange}
+                    modules={quillModules}
+                    formats={quillFormats}
+                    className="mt-4 h-[80%] w-full bg-white"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10 flex justify-end pt-3">
+              <Button type="submit" loading={form.formState.isSubmitting}>
+                Update Post
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
