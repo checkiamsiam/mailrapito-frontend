a2ee39931add251e907b43fa856e37d6d802fcba
pro-merndev <pro.merndev3@gmail.com>
2024-06-01 23:48:48 +0600

    refactor: Update ForwardingModal layout and styles

--- apps/web/modules/marketing/home/modals/ForwardingModal.tsx
+++ apps/web/modules/marketing/home/modals/ForwardingModal.tsx
@@ -33,13 +33,69 @@ const ForwardingModal = () => {
         onOverlayClick={() => setOpen(false)}
         classNames={{
           modal:
-            "z-[100]  m-0  my-auto  p-10 xl:min-w-[1107px] mx-auto rounded-3xl",
+            "z-[100]  m-0  my-auto  p-10 xl:min-w-[1107px] md:min-w-[90vw] mx-auto rounded-3xl",
         }}
       >
         <div>
           <h5 className="mb-2 text-[28px] font-semibold">Forwarding</h5>
-          <p className="text-[#868E96] max-sm:hidden">Forwarding service redirects incoming emails to your email address, making it easier to stay organized and not miss important messages.</p>
-          <p className="text-[#868E96] sm:hidden">Input your real email and choose duration</p>
+          <p className="text-[#868E96] max-sm:hidden">
+            Forwarding service redirects incoming emails to your email address,
+            making it easier to stay organized and not miss important messages.
+          </p>
+          <p className="text-[#868E96] sm:hidden">
+            Input your real email and choose duration
+          </p>
+          <div className="mt-6 overflow-hidden rounded-md border  ">
+            <div className="grid grid-cols-12 ">
+              {/* header part */}
+              <div className="col-span-4">
+                <p className="bg-[#F8F9FA] px-7 py-2 text-[20px] text-[#7C7D81]">
+                  From
+                </p>
+              </div>
+              <div className="col-span-4">
+                <p className="bg-[#F8F9FA] px-7 py-2  text-[20px] text-[#7C7D81]">
+                  Redirected to
+                </p>
+              </div>
+              <div className="col-span-3">
+                <p className="bg-[#F8F9FA] px-7 py-2 text-[20px] text-[#7C7D81] ">
+                  Expire in
+                </p>
+              </div>
+              <div className="col-span-1 bg-[#F8F9FA]" />
+              {/* items loop  */}
+              {[1].map((item) => (
+                <>
+                  <div className="col-span-4 border-b ">
+                    <p className=" px-7 py-4 text-[20px] ">
+                      randomjack123@domain1.com
+                    </p>
+                  </div>
+                  <div className="col-span-4 border-b">
+                    <p className=" px-7 py-4 text-[20px] ">
+                      randomjack123@gmail.com
+                    </p>
+                  </div>
+                  <div className="col-span-3 border-b">
+                    <p className=" px-7 py-4 text-[20px] font-semibold">
+                      3 month
+                    </p>
+                  </div>
+                  <div className="col-span-1 flex items-center justify-center border-b">
+                    <button className="flex items-center justify-center">
+                      <Image
+                        src="/images/icons/delete.svg"
+                        alt="delete"
+                        width={20}
+                        height={20}
+                      />
+                    </button>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
         </div>
       </Modal>
     </div>
