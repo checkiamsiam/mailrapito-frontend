56f343389e72585a309f19a20a01b642d048735d
pro-merndev <pro.merndev3@gmail.com>
2024-06-02 18:21:19 +0600

    refactor: Fix typo in MarkCircleIcon component's clipPath attribute

--- apps/web/modules/shared/icons/MarkCircleIcon.tsx
+++ apps/web/modules/shared/icons/MarkCircleIcon.tsx
@@ -16,7 +16,7 @@ export default function MarkCircleIcon({
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_1_832)">
+      <g clipPath="url(#clip0_1_832)">
         <path
           d="M8 12L11 15L17 9M22.5 12C22.5 17.5228 18.0228 22 12.5 22C6.97715 22 2.5 17.5228 2.5 12C2.5 6.47715 6.97715 2 12.5 2C18.0228 2 22.5 6.47715 22.5 12Z"
           stroke={color}
